32f770a8bab570b6ed14b86865a32f815f013da7
mikaellybarth <wwwmikasouza06@gmail.com>
2023-08-31 16:50:16 -0400

    Commit eba eba

---
+++ apps.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Olá, Mundo!' });
+});
+
+app.get('/produtos', (req, res) => {
+    res.render('produtos', { message: 'qualquer coisa, eba eba eba!!!!' });
+});
+  
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
