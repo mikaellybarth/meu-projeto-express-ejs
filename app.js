e4b0a5a43dc4e538cc6974c85701d7f5455e8390
mikaellybarth <wwwmikasouza06@gmail.com>
2023-11-16 16:24:39 -0400

    commit arrumandoooooo

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ app.use(express.static('public'));
 
 
 const produtos = [
-  {id: 1, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Whiskas", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 1, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 2, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 3, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 4, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 5, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 6, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 7, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 8, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
+  {id: 9, nome: "Ração Whiskas", preço: "R$179,90", descricao: "Ração Premium 100% completa e balanceada para gatos adultos.", imagem: "https://m.media-amazon.com/images/I/81IuLVVQq5L._AC_SY879_.jpg" },
 ]
+
+function buscarProdutoPorID(){
+  const produto = produtos.find (produto => produto.id == 3);
+  return produto || null 
+
+}
 
 app.get('/produtos', (req, res) => {
   res.render('produtos', { message: 'Bem vindo a pagina produtos!' });
